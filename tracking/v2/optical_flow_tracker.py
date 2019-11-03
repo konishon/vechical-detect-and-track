af35147679421dc6b11273a0a7d5b31b46bc8d90
Nishon Tandukar <nishon.tan@gmail.com>
2019-11-03 17:13:59 +0545

    Adds camera class; all fpass code in it

---
+++ tracking/v2/optical_flow_tracker.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+
+class OpticalFlowTracker:
+    tracked_points = []
+
+    def __init__(self):
+        self.old_point = None
+        self.old_gray = None
+        self.x = None
+        self.y = None
+
+    def get_tracked_points(self):
+        return self.tracked_points
+
+    def start_track(self, rgb, points):
+        self.old_point = np.array([[points[0], points[1]]], dtype=np.float32)
+        self.old_gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+
+    def get_position(self):
+        return self.x, self.y
+
+    def update(self, rgb):
+        new_gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+
+        lk_params = dict(winSize=(15, 15),
+                         maxLevel=4,
+                         criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+        new_points, status, error = cv2.calcOpticalFlowPyrLK(self.old_gray, new_gray, self.old_point, None, **lk_params)
+        self.x, self.y = new_points.ravel()
+
+        self.old_gray = new_gray.copy()
+        self.old_point = new_points.reshape(-1, 1, 2)
+        self.tracked_points.append(self.old_point)
---
+++ tracking/v2/retina_net_camera.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+from aerial_pedestrian_detection.keras_retinanet.utils.image import preprocess_image
+from tracking.retinahelper import load_model
+
+'''
+Takes model and video and outputs detections for each frame
+'''
+class RetinaNetCamera:
+    def __init__(self, video_source, show_bb=True):
+        self.cap = cv2.VideoCapture(video_source)
+        self.model = load_model()
+        self.show_bb = show_bb
+
+    def get_frame(self):
+        s, img = self.cap.read()
+        if s:
+            pass
+        img, bb = self.forward_pass(img)
+        return img, bb
+
+    def forward_pass(self, frame):
+        frame = preprocess_image(frame)
+        boxes, scores, labels = self.model.predict_on_batch(
+            np.expand_dims(frame, axis=0))
+
+        if self.show_bb:
+            self.draw_bb(boxes, frame)
+
+        return frame, (boxes, scores, labels)
+
+    @staticmethod
+    def draw_bb(faces, img):
+        padding = 50
+
+        for (x, y, w, h) in faces:
+            x1 = x - padding
+            y1 = y - padding
+            x2 = x + w + padding
+            y2 = y + h + padding
+
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+        return img
